46b5b3b96a4bb5e172de53e5feaf807659d9d399
Will Riffe <w_riffe@protonmail.com>
2023-07-22 21:32:16 -0400

    🏗️ Constructed the reaction model

---
+++ models/Reactions.js
@@ -0,0 +1,43 @@
+// destructure/declare required modules
+const { Schema, Types } = require('mongoose');
+const dateFormat = require('../utils/dateUtil');
+
+
+/* 
+    schema for our reactions
+*/
+const reactions = new Schema(
+  {
+    // assigns unique id to reactions
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    // contains the message of the reaction (i.e. 'the body')
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 250
+    },
+    // contains name of the reactions user/owner
+    username: {
+      type: String,
+      required: true
+    },
+    // formats the timestamp from our date-fns based util
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp, 'yyyy-MM-dd HH:mm:ss')
+    }
+  },
+  {
+    // Includes virtual properties in JSON output
+    toJSON: {
+      getters: true
+    },
+    id: false // isabled because we use reactionId...
+  }
+);
+
+module.exports = reactions;
